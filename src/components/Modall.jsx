c118f6f70954b2dfe8c12368ee876333488f5ac9
Abuabakar Diallo <dialloabubakar5861@gmail.com>
2020-10-30 18:57:56 +0100

    chore: minor changes on the NavModal

--- src/components/Modall.jsx
+++ src/components/Modall.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-modal';
 
+const modalClassNames = 'p-2 my-2 rounded bg-normal-green text-white focus:outline-none focus:bg-darker-green hover:shadow-xl';
+
 function Modall({ modalIsOpen, setModalIsOpen }) {
   return (
     <Modal
@@ -19,23 +21,42 @@ function Modall({ modalIsOpen, setModalIsOpen }) {
         },
         content: {
           position: 'absolute',
-          top: '30%',
+          top: '0',
           left: '0',
-          right: '30%',
+          right: '0',
           bottom: '0',
-          transform: 'translate(20%, -30%)',
-          border: '1px solid #ccc',
+          border: '1px solid #ebecf0',
+          boxShadow: '0px 48px 48px rgba(37, 47, 137, 0.08)',
           background: '#fff',
+          margin: '3rem auto',
           overflow: 'auto',
           WebkitOverflowScrolling: 'touch',
-          borderRadius: '15px',
+          borderRadius: '8px',
+          maxWidth: '50vw',
           outline: 'none',
-          padding: '20px',
+          padding: '25px 20px',
+          height: 'max-content',
         },
       }}
     >
-      <h2>Modal title</h2>
-      <p>Modal boday</p>
+      <div className="content-area">
+        <div className="h-auto">
+          <img src="/images/facebook-login.png" alt="" className="object-cover max-w-full" />
+        </div>
+        <div className="project-info pt-8">
+          <h2 className="text-2xl">
+            Keeping track of hundreds of
+            components
+          </h2>
+
+          <button type="button" className={`${modalClassNames} mr-3 flex`}>
+            See live
+            <img src="/vectors/live.svg" alt="see live" />
+          </button>
+          <button type="button" className={modalClassNames}>See Source</button>
+
+        </div>
+      </div>
     </Modal>
   );
 }
--- src/components/NavModal.jsx
+++ src/components/NavModal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-modal';
 
-const linkClassNames = 'leading-10 pb-12 text-5xl font-semibold';
+const linkClassNames = 'leading-10 text-3xl font-semibold border-b-2 mb-4 pb-2';
 
 function NavModal({ modalIsOpen, setModalIsOpen }) {
   const handleClick = pageName => {
@@ -17,10 +17,9 @@ function NavModal({ modalIsOpen, setModalIsOpen }) {
         overlay: {
           position: 'fixed',
           top: 0,
-          left: 0,
+          left: '100px',
           right: 0,
           bottom: 0,
-          backgroundColor: 'rgba(255, 255, 255, 0.75)',
         },
         content: {
           position: 'absolute',
@@ -30,20 +29,23 @@ function NavModal({ modalIsOpen, setModalIsOpen }) {
           bottom: '0',
           border: 'none',
           background: '#fff',
-          overflow: 'auto',
           WebkitOverflowScrolling: 'touch',
           borderRadius: '0',
+          overflow: 'hidden',
           outline: 'none',
           padding: '20px',
+          maxWidth: '100%',
         },
       }}
     >
-      <div className="flex flex-col justify-center pt-20">
+      <div className="flex flex-col justify-center pt-12">
         <a href="#portfolio" className={linkClassNames} onClick={() => handleClick('#portfolio')}>Portfolio</a>
         <a href="#about" className={linkClassNames} onClick={() => handleClick('#about')}>About</a>
         <a href="#contact" className={linkClassNames} onClick={() => handleClick('#contact')}>Contact</a>
       </div>
-      <button type="button" className="close absolute top-0 right-0 text-5xl pr-5 transform rotate-45 cursor-pointer" onClick={() => setModalIsOpen(false)}>+</button>
+      <button type="button" className="close absolute top-0 right-0 text-5xl mr-3 transform rotate-45 cursor-pointer" onClick={() => setModalIsOpen(false)}>
+        +
+      </button>
     </Modal>
   );
 }
